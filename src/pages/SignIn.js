0f5a22d0ac09bc068679e34af8d24b5e52aa23c1
Nikolay Stoynov <arvigeus@gmail.com>
2018-05-17 17:13:14 +0300

    Add Sign In page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,3 +1,21 @@
+import Button from "components/Input/Button";
+import Field from "components/Input/Field";
 import React from "react";
+import styles from "./SignIn.module.css";
 
-export default () => <div>Sign in</div>;
+const SignIn = () => (
+  <>
+    <form className={styles.email}>
+      <Field
+        type="email"
+        name="email"
+        label="Enter your email to sign in or create an account"
+      />
+      <Button type="submit" value="Sign in" />
+    </form>
+    <span>OR</span>
+    <Button className={styles.facebook}>Sign in with Facebook</Button>
+  </>
+);
+
+export default SignIn;
