a83eae363156664a40cd8ebff022a0f131627d2f
Nikolay Stoynov <arvigeus@gmail.com>
2018-05-17 15:07:00 +0300

    Add About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,63 @@
 import React from "react";
 
-export default () => <div>About</div>;
+const About = () => (
+  <>
+    <h3>
+      <strong>*sigh*</strong> What is the punchline this time?
+    </h3>
+    <p>
+      <em>Digital Diary Cue</em> is not a social network, it's a social
+      framework.
+    </p>
+    <h3>
+      <strong>*rolling eyes*</strong> How exciting...
+    </h3>
+    <p>
+      Unlike your typical social website, this place is not all about you, nor
+      having 500 or more people in your contacts. We want to help you actually
+      build friendships. Each person you add to your contacts is considered a
+      goal.
+    </p>
+    <h3>A goal?</h3>
+    <p>
+      Yes. To keep a healthy relationship with someone usually you need to
+      communicate and show that you care. Now it's easier than ever to keep
+      track with your friends, yet more and more people are feeling lonely and
+      isolated. Why? Because not everyone is interested of what happens in the
+      life of someone they barely know. People want to be heard, not to listen
+      how succesful their friends are. This is all you can do on social media -
+      brag about yourself. You can't do that here. We want to give you the tools
+      needed for connecting to the people you actually care about, or you want
+      to know better.
+    </p>
+    <h3>How would you do that?</h3>
+    <p>
+      There are numerous books about socializing and talking to people. All time
+      classics like Dale Carnegie's{" "}
+      <em>„How to Win Friends and Influence People“</em> provide great insights.
+      It is surprising that nobody brought those practices to our digital age.
+      We have all needed tools - chat apps, orginizers, todo lists, calendaes,
+      why not combine their power for this single purpose?
+    </p>
+    <h3>
+      All my friends are already on some social network. I can't simply convince
+      them to ditch their service for yours...
+    </h3>
+    <p>
+      You don't have to! That's why many „Facebook killer“ services failed. They
+      all did the same that Facebook did - better or not. Why would you need two
+      of the same thing if what you have is already doing decent job? Here comes
+      the beauty of our service - you can use it even if none of your friends is
+      using it. Just add them by their email address and you are done. The only
+      feature you won't be able to use is our chat app and its associated
+      functions. Besides that, your experience will remain the same. When they
+      join, you can connect and reuse all information you already have.
+    </p>
+    <p>
+      We are not trying to replace Facebook or other - actually we believe that
+      we all could benefit from each other!
+    </p>
+  </>
+);
+
+export default About;
