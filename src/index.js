c7ae5e3abeaa1e8b163bb1181803530f8f9a10b0
Nikolay Stoynov <arvigeus@gmail.com>
2018-05-17 12:00:32 +0300

    Introduce Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({
+  uri: `${process.env.REACT_APP_SERVER_URL}/graphql`
+});
+
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp(App), document.getElementById("root"));
 registerServiceWorker();
