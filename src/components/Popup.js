a87c2c714c69964116d58c899bfb7cb950867c17
Nikolay Stoynov <arvigeus@gmail.com>
2018-05-29 17:54:49 +0300

    Add Popup title; change overlay color

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,25 @@
+// @flow
+import cx from "classnames";
 import CloseIcon from "components/icons/Close";
 import React from "react";
 import styles from "./Popup.module.css";
 
-const Popup = ({ cancel, children }) => (
+type PopupPropsType = {
+  title?: string,
+  cancel: () => void,
+  children: any
+};
+
+const Popup = ({ title, cancel, children }: PopupPropsType) => (
   <div className={styles.container}>
     <div className={styles.overlay} onClick={cancel} />
     <div className={styles.popup}>
-      <CloseIcon size={28} className={styles.close} onClick={cancel} />
+      <div className={styles.header}>
+        <div className={cx(styles.title, { [styles["title-line"]]: !!title })}>
+          {title}
+        </div>
+        <CloseIcon size={28} className={styles.close} onClick={cancel} />
+      </div>
       {children}
     </div>
   </div>
