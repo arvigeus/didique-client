6fcbed6205fa7c60b394c4ecb90a64ede9f1f27c
Nikolay Stoynov <arvigeus@gmail.com>
2018-05-31 13:07:46 +0300

    Separate Popup to Popup and Note components

---
+++ src/components/Note.js
@@ -0,0 +1,28 @@
+// @flow
+import cx from "classnames";
+import CloseIcon from "components/icons/Close";
+import React from "react";
+import styles from "./Note.module.css";
+
+type NotePropsType = {
+  className?: string,
+  title?: string,
+  cancel?: () => void,
+  children: any
+};
+
+const Note = ({ className, title, cancel, children }: NotePropsType) => (
+  <div className={cx(styles.note, className)}>
+    {title && cancel ? (
+      <div className={styles.header}>
+        <div className={cx(styles.title, { [styles["title-line"]]: !!title })}>
+          {title}
+        </div>
+        <CloseIcon size={28} className={styles.close} onClick={cancel} />
+      </div>
+    ) : null}
+    {children}
+  </div>
+);
+
+export default Note;
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,27 +1,19 @@
 // @flow
-import cx from "classnames";
-import CloseIcon from "components/icons/Close";
+import Note from "components/Note";
 import React from "react";
 import styles from "./Popup.module.css";
 
 type PopupPropsType = {
-  title?: string,
   cancel: () => void,
   children: any
 };
 
-const Popup = ({ title, cancel, children }: PopupPropsType) => (
+const Popup = ({ cancel, children, ...props }: PopupPropsType) => (
   <div className={styles.container}>
     <div className={styles.overlay} onClick={cancel} />
-    <div className={styles.popup}>
-      <div className={styles.header}>
-        <div className={cx(styles.title, { [styles["title-line"]]: !!title })}>
-          {title}
-        </div>
-        <CloseIcon size={28} className={styles.close} onClick={cancel} />
-      </div>
+    <Note className={styles.popup} cancel={cancel} {...props}>
       {children}
-    </div>
+    </Note>
   </div>
 );
 
