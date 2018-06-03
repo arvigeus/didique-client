b62359ab7a7a8e7c34f851f859f7673e8a30e0e1
Nikolay Stoynov <arvigeus@z170i.localdomain>
2018-06-03 23:05:45 +0300

    Friend page mock

---
+++ src/pages/Friend/components/Paperclip.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Paperclip.module.css";
+
+const Paperclip = () => (
+  <>
+    <div className={styles["paperclip-top"]} />
+    <div className={styles["paperclip-bottom"]} />
+  </>
+);
+
+export default Paperclip;
